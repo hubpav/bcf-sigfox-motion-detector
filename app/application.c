5a830e5bbc3dfe2825188ffd465bfd3c2d1966e3
Pavel Hübner <pavel.hubner@bigclown.com>
2017-02-27 10:51:22 +0100

    Testing SigFox application

--- app/application.c
+++ app/application.c
@@ -1,81 +1,37 @@
-/*
-
-Visual Studio Code
-Ctrl+Shift+B     to build
-Ctrl+P task dfu  to flash MCU with dfu-util
-
-*/
-
 #include <application.h>
-#include <usb_talk.h>
-#include <bc_led.h>
-#include <bc_button.h>
-#include <bc_i2c.h>
-#include <bc_tag_temperature.h>
-#include <bc_tag_humidity.h>
-#include <bc_tag_lux_meter.h>
-#include <bc_tag_barometer.h>
-#include <bc_lis2dh12.h>
 
-#include <bc_spirit1.h>
-
-struct
-{
-    struct { bool valid; float value; } temperature;
-    struct { bool valid; float value; } humidity;
-    struct { bool valid; float value; } humidity_r1;
-    struct { bool valid; float value; } luminosity;
-    struct { bool valid; float value; } altitude;
-    struct { bool valid; float value; } pressure;
-    struct { bool valid; float value; } acceleration_x;
-    struct { bool valid; float value; } acceleration_y;
-    struct { bool valid; float value; } acceleration_z;
-
-} i2c_sensors;
-
+// LED instance
 bc_led_t led;
 
-static void spirit1_event_handler(bc_spirit1_event_t event, void *event_param);
+// Button instance
+bc_button_t button;
 
-void temperature_tag_event_handler(bc_tag_temperature_t *self, bc_tag_temperature_event_t event, void *event_param)
+// Temperature Tag instance
+bc_tag_temperature_t temperature_tag;
+
+// SigFox Module instance
+bc_module_sigfox_t sigfox_module;
+
+// Variable holding temperature in centigrades
+float temperature;
+
+void application_init(void)
 {
-    (void) event;
-    (void) event_param;
+    // Initialize LED
+    bc_led_init(&led, BC_GPIO_LED, false, false);
 
-    i2c_sensors.temperature.valid = bc_tag_temperature_get_temperature_celsius(self, &i2c_sensors.temperature.value);
-}
+    // Initialize button
+    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
+    bc_button_set_event_handler(&button, button_event_handler, NULL);
 
-void humidity_tag_event_handler(bc_tag_humidity_t *self, bc_tag_humidity_event_t event, void *event_param)
-{
-    (void) event;
-    (void) event_param;
+    // Initialize Temperature Tag
+    bc_tag_temperature_init(&temperature_tag, BC_I2C_I2C0, BC_TAG_TEMPERATURE_I2C_ADDRESS_ALTERNATE);
+    bc_tag_temperature_set_update_interval(&temperature_tag, 10000);
+    bc_tag_temperature_set_event_handler(&temperature_tag, temperature_tag_event_handler, NULL);
 
-    i2c_sensors.humidity.valid = bc_tag_humidity_get_humidity_percentage(self, &i2c_sensors.humidity.value);
-}
-
-void humidity_tag_event_handler_r1(bc_tag_humidity_t *self, bc_tag_humidity_event_t event, void *event_param)
-{
-    (void) event;
-    (void) event_param;
-
-    i2c_sensors.humidity_r1.valid = bc_tag_humidity_get_humidity_percentage(self, &i2c_sensors.humidity_r1.value);
-}
-
-void lux_meter_event_handler(bc_tag_lux_meter_t *self, bc_tag_lux_meter_event_t event, void *event_param)
-{
-    (void) event;
-    (void) event_param;
-
-    i2c_sensors.luminosity.valid = bc_tag_lux_meter_get_luminosity_lux(self, &i2c_sensors.luminosity.value);
-}
-
-void barometer_tag_event_handler(bc_tag_barometer_t *self, bc_tag_barometer_event_t event, void *event_param)
-{
-    (void) event;
-    (void) event_param;
-
-    i2c_sensors.altitude.valid = bc_tag_barometer_get_altitude_meter(self, &i2c_sensors.altitude.value);
-    i2c_sensors.pressure.valid = bc_tag_barometer_get_pressure_pascal(self, &i2c_sensors.pressure.value);
+    // Initialize SigFox Module
+    bc_module_sigfox_init(&sigfox_module, BC_MODULE_SIGFOX_REVISION_R2);
+    bc_module_sigfox_set_event_handler(&sigfox_module, sigfox_module_event_handler, NULL);
 }
 
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
@@ -83,138 +39,89 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
     (void) self;
     (void) event_param;
 
-    if (event == BC_BUTTON_EVENT_PRESS)
+    if (event == BC_BUTTON_EVENT_CLICK)
     {
-        bc_led_pulse(&led, 100);
+        if (bc_module_sigfox_is_ready(&sigfox_module))
+        {
+            uint8_t buffer[] = { 0x01 };
 
-        bc_spirit1_set_tx_length(16);
-        bc_spirit1_tx();
-
-        /*
-        static uint16_t event_count = 0;
-
-        bc_led_set_mode(&led, (event_count & 1) != 0 ? BC_LED_MODE_BLINK : BC_LED_MODE_OFF);
-
-        usb_talk_publish_push_button("", &event_count);
-
-        event_count++;
-        */
+            bc_module_sigfox_send_rf_frame(&sigfox_module, buffer, sizeof(buffer));
+        }
     }
 
-    if (event == BC_BUTTON_EVENT_RELEASE)
+    if (event == BC_BUTTON_EVENT_HOLD)
     {
-        bc_led_pulse(&led, 100);
+        bc_module_sigfox_continuous_wave(&sigfox_module);
     }
 }
 
-void lis2dh12_event_handler(bc_lis2dh12_t *self, bc_lis2dh12_event_t event, void *event_param)
+void temperature_tag_event_handler(bc_tag_temperature_t *self, bc_tag_temperature_event_t event, void *event_param)
 {
     (void) event_param;
 
-    if (event == BC_LIS2DH12_EVENT_UPDATE)
+    if (event == BC_TAG_TEMPERATURE_EVENT_UPDATE)
     {
-        bc_lis2dh12_result_g_t result;
-
-        if (bc_lis2dh12_get_result_g(self, &result))
+        if (bc_module_sigfox_is_ready(&sigfox_module))
         {
-            i2c_sensors.acceleration_x.value = result.x_axis;
-            i2c_sensors.acceleration_y.value = result.y_axis;
-            i2c_sensors.acceleration_z.value = result.z_axis;
+            // Read temperature
+            bc_tag_temperature_get_temperature_celsius(self, &temperature);
 
-            i2c_sensors.acceleration_x.valid = true;
-            i2c_sensors.acceleration_y.valid = true;
-            i2c_sensors.acceleration_z.valid = true;
+            uint8_t buffer[5];
+
+            buffer[0] = 0x02;
+            buffer[1] = (uint32_t) temperature;
+            buffer[2] = (uint32_t) temperature >> 8;
+            buffer[3] = (uint32_t) temperature >> 16;
+            buffer[4] = (uint32_t) temperature >> 24;
+
+            bc_module_sigfox_send_rf_frame(&sigfox_module, buffer, sizeof(buffer));
         }
-        else
-        {
-            i2c_sensors.acceleration_x.valid = false;
-            i2c_sensors.acceleration_y.valid = false;
-            i2c_sensors.acceleration_z.valid = false;
-        }
-    }
-    else if (event == BC_LIS2DH12_EVENT_ALARM)
-    {
-        bc_led_pulse(&led, 100);
     }
 }
 
-bc_tag_humidity_t humidity_tag;
-bc_tag_humidity_t humidity_tag_r1;
-
-void application_init(void)
+void sigfox_module_event_handler(bc_module_sigfox_t *self, bc_module_sigfox_event_t event, void *event_param)
 {
-    usb_talk_init();
-
-    bc_led_init(&led, BC_GPIO_LED, false, false);
-    //bc_led_set_mode(&led, BC_LED_MODE_BLINK);
-
-    static bc_button_t button;
-
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    bc_button_set_event_handler(&button, button_event_handler, NULL);
-
-    bc_i2c_init(BC_I2C_I2C0, BC_I2C_SPEED_400_KHZ);
-
-    static bc_tag_temperature_t temperature_tag;
-
-    bc_tag_temperature_init(&temperature_tag, BC_I2C_I2C0, BC_TAG_TEMPERATURE_I2C_ADDRESS_ALTERNATE);
-    bc_tag_temperature_set_update_interval(&temperature_tag, 1000);
-    bc_tag_temperature_set_event_handler(&temperature_tag, temperature_tag_event_handler, NULL);
-
-    bc_tag_humidity_init(&humidity_tag, BC_TAG_HUMIDITY_REVISION_R2, BC_I2C_I2C0, BC_TAG_HUMIDITY_I2C_ADDRESS_DEFAULT);
-    bc_tag_humidity_set_update_interval(&humidity_tag, 1000);
-    bc_tag_humidity_set_event_handler(&humidity_tag, humidity_tag_event_handler, NULL);
-
-    bc_tag_humidity_init(&humidity_tag_r1, BC_TAG_HUMIDITY_REVISION_R1, BC_I2C_I2C0, BC_TAG_HUMIDITY_I2C_ADDRESS_DEFAULT);
-    bc_tag_humidity_set_update_interval(&humidity_tag_r1, 1000);
-    bc_tag_humidity_set_event_handler(&humidity_tag_r1, humidity_tag_event_handler_r1, NULL);
-
-    static bc_tag_lux_meter_t lux_meter;
-
-    bc_tag_lux_meter_init(&lux_meter, BC_I2C_I2C0, BC_TAG_LUX_METER_I2C_ADDRESS_DEFAULT);
-    bc_tag_lux_meter_set_update_interval(&lux_meter, 1000);
-    bc_tag_lux_meter_set_event_handler(&lux_meter, lux_meter_event_handler, NULL);
-
-    static bc_tag_barometer_t barometer_tag;
-
-    bc_tag_barometer_init(&barometer_tag, BC_I2C_I2C0);
-    bc_tag_barometer_set_update_interval(&barometer_tag, 1000);
-    bc_tag_barometer_set_event_handler(&barometer_tag, barometer_tag_event_handler, NULL);
-
-    static bc_lis2dh12_t lis2dh12;
-
-    bc_lis2dh12_alarm_t alarm;
-
-    alarm.x_high = false;
-    alarm.x_low = false;
-    alarm.y_high = false;
-    alarm.y_low = false;
-    alarm.z_high = true;
-    alarm.z_low = false;
-    alarm.threshold = .5f;
-    alarm.duration = 1;
-
-    bc_lis2dh12_init(&lis2dh12, BC_I2C_I2C0, 0x19);
-    bc_lis2dh12_set_update_interval(&lis2dh12, 100);
-    bc_lis2dh12_set_alarm(&lis2dh12, &alarm);
-    bc_lis2dh12_set_event_handler(&lis2dh12, lis2dh12_event_handler, NULL);
-
-    bc_spirit1_init();
-    bc_spirit1_set_event_handler(spirit1_event_handler, NULL);
-    bc_spirit1_rx();
-}
-
-static void spirit1_event_handler(bc_spirit1_event_t event, void *event_param)
-{
+    (void) self;
     (void) event_param;
 
-    if (event == BC_SPIRIT1_EVENT_RX_DONE)
-    {
-        bc_led_pulse(&led, 100);
-    }
+    static bool has_device_id = false;
+    static bool has_device_pac = false;
 
-    if (event == BC_SPIRIT1_EVENT_TX_DONE)
+    // If event is ready...
+    if (event == BC_MODULE_SIGFOX_EVENT_READY)
     {
-        bc_spirit1_rx();
+        if (!has_device_id)
+        {
+            bc_module_sigfox_read_device_id(self);
+        }
+        else if (!has_device_pac)
+        {
+            bc_module_sigfox_read_device_pac(self);
+        }
+    }
+    // If event is start of transmission...
+    else if (event == BC_MODULE_SIGFOX_EVENT_ERROR)
+    {
+        bc_led_set_mode(&led, BC_LED_MODE_BLINK);
+    }
+    // If event is start of transmission...
+    else if (event == BC_MODULE_SIGFOX_EVENT_SEND_RF_FRAME_START)
+    {
+        bc_led_set_mode(&led, BC_LED_MODE_ON);
+    }
+    // If event is end of transmission...
+    else if (event == BC_MODULE_SIGFOX_EVENT_SEND_RF_FRAME_DONE)
+    {
+        bc_led_set_mode(&led, BC_LED_MODE_OFF);
+    }
+    // If event is read device ID...
+    else if (event == BC_MODULE_SIGFOX_EVENT_READ_DEVICE_ID)
+    {
+        has_device_id = true;
+    }
+    // If event is read device PAC...
+    else if (event == BC_MODULE_SIGFOX_EVENT_READ_DEVICE_PAC)
+    {
+        has_device_pac = true;
     }
 }
