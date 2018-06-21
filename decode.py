8254daf9a5d54755993f627a10aa1622f00ee28a
Karel Blavka <karel.blavka@bigclown.com>
2018-06-21 10:36:14 +0200

    Update decode.py

--- decode.py
+++ decode.py
@@ -23,43 +23,46 @@ def temperature_decode(txt):
 
 def decode(data):
     length = len(data)
-    if length != 6 and length != 10:
-        raise Exception("Bad data length, 6 or 10 characters expected")
+    if length < 10:
+        raise Exception("Bad data length, min 10 characters expected")
 
     header = int(data[0], 16)
 
     return {
         "header": header_lut[header],
         "position": int(data[1], 16),
-        "voltage": int(data[2], 16) / 8.0 + 1.8,
+        "voltage": int(data[2], 16) / 4.0 + 2,
         "temperature": temperature_decode(data[4:6]),
-        "count": int(data[6:10], 16) if header == SIGFOX_HEADER_ALERT_MOTION else None,
+        "count": int(data[6:10], 16),
         "temperature_alert": temperature_decode(data[6:8]) if header == SIGFOX_HEADER_ALERT_TEMPERATURE else None,
-        "temperature_previous": temperature_decode(data[8:10]) if header == SIGFOX_HEADER_ALERT_TEMPERATURE else None
+        "temperature_previous": temperature_decode(data[8:10]) if header == SIGFOX_HEADER_ALERT_TEMPERATURE else None,
+        "humidity":  int(data[10:12], 16) / 2.0 if len(data) >= 12 else None,
+        "illuminance":  int(data[12:16], 16) * 2.0 if len(data) >= 16 else None,
+        "pressure":  int(data[16:20], 16) * 2.0 if len(data) >= 20 else None
     }
 
 
 def pprint(data):
     print('Message :', data['header'])
     print('Position :', data['position'])
-    print('Voltage :', data['voltage'])
-    print('Temperature :', data['temperature'])
+    print('Voltage :', data['voltage'], 'V')
+    print('Temperature :', data['temperature'], '°C')
     if data['count'] is not None:
         print('Event count :', data['count'])
     if data['temperature_alert'] is not None:
-        print('Temperature alert :', data['temperature_alert'])
+        print('Temperature alert :', data['temperature_alert'], '°C')
     if data['temperature_previous'] is not None:
-        print('Temperature previous :', data['temperature_previous'])
+        print('Temperature previous :', data['temperature_previous'], '°C')
+
+    print("Humidity :", data["humidity"], '%')
+    print("Illuminance :", data["illuminance"], 'lux')
+    print("Pressure :", data["pressure"], 'Pa')
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2 or sys.argv[1] in ('help', '-h', '--help'):
         print("usage: python3 decode.py [data]")
-        print("example BOOT paket: python3 decode.py 014062")
-        print("example BEACON paket: python3 decode.py 168064")
-        print("example ALERT MOTION paket: python3 decode.py 2150620001")
-        print("example ALERT MOTION paket: python3 decode.py 2150620000")
-        print('example ALERT TEMPERATURE paket: python3 decode.py 3350667262')
+        print("example ALERT MOTION paket: python3 decode.py 21e06b0001650003c1c8")
         exit(1)
 
     data = decode(sys.argv[1])
